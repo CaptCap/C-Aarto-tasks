824c418c328e43fa7d8052eb9b82b1df1b667957
Trapeznikov Vasyl <>
2022-11-20 16:18:29 +0200

    task done

---
+++ Pointgame/Program.cs
@@ -0,0 +1,25 @@
+﻿int farms;
+int duce;
+int province;
+Console.WriteLine("Enter farms");
+while(!int.TryParse(Console.ReadLine(), out farms ))
+{
+    Console.WriteLine("not a number");
+}
+
+Console.WriteLine("Enter duces");
+while (!int.TryParse(Console.ReadLine(), out duce))
+{
+    Console.WriteLine("not a number");
+}
+Console.WriteLine("Enter provinces");
+while (!int.TryParse(Console.ReadLine(), out province))
+{
+    Console.WriteLine("not a number");
+
+}
+
+int result = farms * 1 + duce * 3 + province * 6;
+Console.WriteLine("Total score is: " + result);
+Console.ReadKey();
+
